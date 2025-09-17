908524355e562f69f6864323454286fdd1b7486e
MelMarvel <melody.okogun@reventtechnologies.com>
2025-09-17 16:58:51 +0100

    added codemagic.yaml

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -11,11 +22,13 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+plugins {
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
